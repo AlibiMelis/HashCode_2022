81c2083a5ba6a3661e119c3cbbdf51f4989ebbc5
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-02-25 05:55:25 +0800

    final

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import Output from './output';
 import { readInput } from './read';
 import ScheduleProjects from './scheduling';
 
-const {people, projects} = readInput('./samples/b.txt');
+const {people, projects} = readInput('./samples/a.txt');
 
 const assignments = ScheduleProjects(projects, people);
 // console.log(assignments);
